1a396d3e3387a35f1e5f0a1e750b86793a8dba1d
freshprinceofcoders <milanosolanki27@gmail.com>
2020-06-15 17:55:28 +0100

    official working code needs to change the woring in the  strings

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
+steps = 0
 gameOverCondition = False
 option = ""
+
 
 name = input("Enter your name you pillock: ")
 print(f" Welcome {name} lets get ready to go on a quest\n you find yourself next haunted house with treasure HAHA!.\n Can you find your way to the treasure?")
@@ -15,30 +17,73 @@ else:
     print("I don't understand")
     exit()
 
-while gameOverCondition==False:
+while gameOverCondition == False:
     if option=="":
         selection = input("Enter a room: ")
-
+    if option != "":
+        selection == "k"
     if selection=="k":
         print("\nwelcome to the kitchen. Now you have two options")
         print("\tOption 1: Select an item to look under treasure\n\t\tf = Fridge\n\t\to = oven\n\t\tm = microwave")
         print("\tOption 2: change the room by selecting room option from welcome instrucitions")
+        steps += 1
         option = input("\tpick an option: ")
-        selection = option
-        if option=="f":
-            print("Congratulations you found the tresure!")
-            gameOverCondition=True
-        if option=="o":
-            print("You're burned by the heat of the oven...get better and try again.")
-            gameOverCondition=True
-        if option=="m":
-            print("Better luch next time try again.")
-       
-
-    if selection=="u":
-        print("\nwelcome to the utility. Now you have two options")
-        print("\tOption 1: Select an item to look under treasure\n\t\tw = Washing machine\n\t\tc = cupboard")
+       # selection = option
+        if option == "f":
+            print("no")
+            gameOverCondition = True
+        if option == "m":
+            print("burnt")
+            gameOverCondition = True
+        if option == "o":
+            print("Try again")
+            
+    
+    if selection == "u":
+        print("\nwelcome to the Utility room. Now you have two options")
+        print("\tOption 1: Select an item to look under treasure\n\t\tb = Bin\n\t\tw = Washing machine\n\t\tc = Cupboard")
         print("\tOption 2: change the room by selecting room option from welcome instrucitions")
+        steps += 1
         option = input("\tpick an option: ")
-        selection = option
-
+        #selection = option
+        if option == "b":
+            print("You have snake sthat are coming out and you have benn biten")
+            gameOverCondition = True
+        if option == "c":
+            print("Spiders are all of you bye")
+            gameOverCondition = True
+        if option == "w":
+            print("Try again Washing machine is running")
+    
+    if selection == "h":
+        print("\nwelcome to the Hallway. Now you have two options")
+        print("\tOption 1: Select an item to look under treasure\n\t\ts = storage cupboard\n\t\tsc = shoe cupboard\n\t\tstr = stairs")
+        print("\tOption 2: change the room by selecting room option from welcome instrucitions")
+        steps += 1
+        option = input("\tpick an option: ")
+        #selection = option
+        if option == "s":
+            print("You got attacked by a bear hinding in the cupboard")
+            gameOverCondition = True
+        if option =="sc":
+            print("Damn there are to many bats and corono here bye")
+            gameOverCondition = True
+        if option == "str":
+            print("Try again you cannot go upstairs")
+            
+    
+    if selection == "l":
+        print("\nwelcome to the Living room. Now you have two options")
+        print("\tOption 1: Select an item to look under treasure\n\t\ts = Sofa\n\t\tt = TV\n\t\tw = Window")
+        print("\tOption 2: change the room by selecting room option from welcome instrucitions")
+        steps += 1
+        option = input("\tpick an option: ")
+        #selection = option
+        if option == "s":
+            print("You found the coin which is priceless well done for the win!!!!")
+            gameOverCondition = True
+        if option =="w":
+            print("Get out of here bye")
+            gameOverCondition = True
+        if option == "t":
+            print("Try again")
